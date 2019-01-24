cda54ff51feb3f9ec0610b186339bad07da1a547
Namita <aroranamita33@gmail.com>
2019-01-24 13:03:22 +0530

    second done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Scenario1Component } from './scenario1/scenario1.component';
+import { Scenario2Component } from './scenario2/scenario2.component';
+import { Scenario3Component } from './scenario3/scenario3.component';
+import { Child1Component } from './scenario3/child1/child1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Scenario1Component
+    Scenario1Component,
+    Scenario2Component,
+    Scenario3Component,
+    Child1Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/scenario2/scenario2.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+@Component({
+  selector: 'app-scenario2',
+  templateUrl: './scenario2.component.html',
+  styleUrls: ['./scenario2.component.css']
+})
+export class Scenario2Component implements OnInit {
+  id = 'Enter some text here';
+  name = 'Namita';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
